ee8e48bbbaa0a20dcc0ec584fce06544d4877d2e
XobazJr <MR.TeerapatKotanartChannel@Gmail.COM>
2024-10-26 02:06:52 +0700

    Update interpolation-and-extrapolation-i.js
    ✅ Add code interpolation and extrapolation i

--- interpolation-and-extrapolation-i/interpolation-and-extrapolation-i.js
+++ interpolation-and-extrapolation-i/interpolation-and-extrapolation-i.js
@@ -5,15 +5,15 @@ function interpolation_and_extrapolation_i(points,n){
 
     for(let i=0;i<points.length;i++){
         let term = points[i].y;
-        let xi = points[i].x;
         for(let j=0;j<points.length;j++){
-            if(j != i){
-                term *= (n - points[j].x) / (xi - points[j].x);
+            if(j !== i){
+                term *= (n - points[j].x) / (points[i].x - points[j].x); 
             }
         }
         sum += term;
     }
-    return parseFloat(sum).toFixed(6);
+
+    return parseFloat(sum.toFixed(6));
 }
 
 let x = [0,20000,40000,60000,80000];
@@ -21,8 +21,8 @@ let y = [9.81,9.7487,9.6879,9.6879,9.5682];
 let n = 46348;
 
 let linear_interpolation = [
-    {x: x[0],y: y[0]},
-    {x: x[4],y: y[4]}
+    {x: x[0], y: y[0]},
+    {x: x[4], y: y[4]},
 ];
 
 let quadratic_interpolation = [
@@ -37,7 +37,7 @@ let polynomial_interpolation = [
     {x: x[2],y: y[2]},
     {x: x[3],y: y[3]},
     {x: x[4],y: y[4]}
-]
+];
 
 console.log(interpolation_and_extrapolation_i(linear_interpolation,n));
 console.log(interpolation_and_extrapolation_i(quadratic_interpolation,n));
